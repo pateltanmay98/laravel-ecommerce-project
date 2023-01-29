876ced7ee7c962e5dd7cf76e7bb4fc4bdb303b85
unknown <patelth1998@gmail.com>
2023-01-29 12:05:28 +0530

    Logout and Cache disable

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Admin Routes
+Route::controller(AdminController::class)->group(function() {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
 });
 
 Route::get('/dashboard', function () {
