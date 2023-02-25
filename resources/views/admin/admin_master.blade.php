990d8e19c4d6861ee344fee2157a07eb923ac158
unknown <patelth1998@gmail.com>
2023-02-25 19:45:29 +0530

    Changes in Admin Profile Page

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -112,7 +112,7 @@
         <div class="rightbar-overlay"></div>
 
         <!-- JAVASCRIPT -->
-        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/pages/jquery.min.js') }}"></script>
         <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
         <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
---
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="page-content">
+    <div class="container-fluid">
+        
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title">Edi Profile Page</h4>
+
+                        <form>
+
+                            <div class="row mb-3">
+                                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input name="name" class="form-control" type="text" value="{{ $editData->name }}" id="name">
+                                </div>
+                            </div> <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="email" class="col-sm-2 col-form-label">User Email</label>
+                                <div class="col-sm-10">
+                                    <input name="email" class="form-control" type="email" value="{{ $editData->email }}" id="email">
+                                </div>
+                            </div> <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="username" class="col-sm-2 col-form-label">Username</label>
+                                <div class="col-sm-10">
+                                    <input name="username" class="form-control" type="text" value="{{ $editData->username }}" id="username">
+                                </div>
+                            </div> <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="image" class="col-sm-2 col-form-label">Profile Image</label>
+                                <div class="col-sm-10">
+                                    <input name="user_image" class="form-control" type="file" id="userimage">
+                                </div>
+                            </div> <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="image" class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <img id="display_image" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                </div>
+                            </div> <!-- end row -->
+
+                            <input type="submit" class="btn btn-info waves-effect" value="Update Profile">
+
+                        </form>
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+
+    </div>
+</div>
+
+<script src="{{ asset('backend/assets/js/pages/jquery.min.js') }}"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#user_image').change(function(e) {
+            var render = new FileReader();
+            render.onload = function(e) {
+                $('#display_image').attr('src', e.target.result);
+            }
+            render.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+
+@endsection
